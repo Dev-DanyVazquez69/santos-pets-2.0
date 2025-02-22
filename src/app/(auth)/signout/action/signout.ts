9e6d49103f37075720b518f901f4a31d9e10d914
Dev-DanyVazquez69 <danyv7818@gmail.com>
2025-02-22 15:54:00 -0300

    initial config auth.ts

--- src/app/(auth)/signout/action/signout.ts
+++ src/app/(auth)/signout/action/signout.ts
@@ -1,18 +1,16 @@
 "use server"
 
 import { signOut } from '@/app/auth'
-import { redirect } from 'next/navigation'
 
 const signout = async () => {
 
     try {
-        await signOut()
+        await signOut({ redirectTo: "/signin" })
     } catch (error) {
         if (error instanceof Error) {
             throw error
         }
     }
-    redirect("/signin")
 }
 
 export default signout;
